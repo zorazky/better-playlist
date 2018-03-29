67de30f151e6e4e31343ff014b5fdd4c39e1e2bc
Oswaldo <oswaldo.vazquez@gmail.com>
2018-03-29 06:55:08 -0600

    initial commit

---
+++ spotify/src/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+let defaultTextColor = "#fff";
+let defaultStyle = {
+  color: defaultTextColor
+};
+
+class Aggregate extends Component {
+  render () {
+    return (
+      <div style={{...defaultStyle, width: "40%", display: "inline-block"}}>
+        <h2>Number Text</h2>
+      </div>
+    );
+  }
+}
+
+class Filter extends Component {
+  render () {
+    return (
+      <div style={defaultStyle}>
+        <img alt=""/>
+        <input type="text" />
+      </div>
+    );
+  }
+}
+
+class Playlist extends Component {
+  render () {
+    return (
+      <div style={{...defaultStyle, width: "25%", display: "inline-block"}}>
+        <img alt=""/>
+        <h3>Playlist Name</h3>
+        <ul>
+          <li>Song 1</li>
+          <li>Song 2</li>
+          <li>Song 3</li>
+        </ul>
+
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <h1>Title</h1>
+        <Aggregate />
+        <Aggregate />
+        <Filter />
+        <Playlist />
+        <Playlist />
+        <Playlist />
+        <Playlist />
+      </div>
+    );
+  }
+}
+
+
+export default App;
