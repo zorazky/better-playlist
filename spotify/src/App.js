4042564c09eaa51a57ad4f678307be7a75281a35
Oswaldo <oswaldo.vazquez@gmail.com>
2018-03-29 07:01:15 -0600

    small style change

--- spotify/src/App.js
+++ spotify/src/App.js
@@ -50,7 +50,7 @@ class App extends Component {
     return (
       <div className="App">
 
-        <h1>Title</h1>
+        <h1 style={{...defaultStyle, "font-size": "54px"}}>Title</h1>
         <Aggregate />
         <Aggregate />
         <Filter />
